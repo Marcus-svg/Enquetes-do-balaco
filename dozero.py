d20e29a44e937337d198ca64a20516a6222ba596
Marcus Pires <marcuspiresdecastro@gmail.com>
2025-11-23 15:47:10 -0300

    Logs(diario de batalha)1

--- dozero.py
+++ dozero.py
@@ -1,1 +1,27 @@
-print("boa noite")
+class GameLogger:
+    _instance = None
+
+    def __new__(cls):
+        
+        if cls._instance is None:
+            cls._instance = super(GameLogger, cls).__new__(cls)
+            cls._instance.history = []
+        return cls._instance
+
+    def log(self, message):
+        print(f"[LOG]: {message}")
+        self.history.append(message)
+
+    def show_history(self):
+        print("\n--- Histórico Completo ---")
+        for event in self.history:
+            print(event)
+
+logger1 = GameLogger()
+logger1.log("Jogo começou.")
+
+logger2 = GameLogger()
+logger2.log("O herói entrou na caverna.")
+
+print(f"\nLogger1 é igual ao Logger2? {logger1 is logger2}") 
+logger1.show_history()
