7cd5d6fcd6da33bbeb321690994b0946419dfec5
Marcus Pires <marcuspiresdecastro@gmail.com>
2025-11-23 16:37:09 -0300

    class weaponbuider

--- dozero.py
+++ dozero.py
@@ -31,3 +31,38 @@ class Weapon:
         self.name = name
         self.damage = damage
         self.element = element
+
+    def __str__(self):
+        return f"[{self.name}] Dano: {self.damage} | Elemento: {self.element}"
+
+class WeaponBuilder:
+    def __init__(self):
+        self._name = "Arma Básica"
+        self._damage = 1
+        self._element = "Normal"
+
+    def set_name(self, name):
+        self._name = name
+        return self
+
+    def set_damage(self, damage):
+        self._damage = damage
+        return self
+
+    def set_element(self, element):
+        self._element = element
+        return self
+
+    def build(self):
+        return Weapon(self._name, self._damage, self._element)
+
+faca = WeaponBuilder().set_name("Faca de Cozinha").build()
+print(faca)
+
+martelo_do_trovao = (WeaponBuilder()
+                     .set_name("Mjolnir")
+                     .set_damage(999)
+                     .set_element("Raio")
+                     .build())
+
+print(martelo_do_trovao)
